7816e2e41209eeac81331e214d099d6fb4e95b6d
Samarth Gupta <samarthgupta1202@gmail.com>
2022-03-13 17:35:10 +0530

    files updated

---
+++ index.js
@@ -0,0 +1,9 @@
+for (let index = 0; index < document.querySelectorAll(".drum").length; index++) {
+    document.querySelectorAll("button")[index].addEventListener("click", function () {
+        alert("i got clicked");
+
+    });
+    
+}
+
+
