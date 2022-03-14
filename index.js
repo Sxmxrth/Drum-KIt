8fac919d3c44b7de77e9d8a3ba28102f00a2330b
Samarth Gupta <samarthgupta1202@gmail.com>
2022-03-14 11:21:14 +0530

    files updated

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-for (let index = 0; index < document.querySelectorAll(".drum").length; index++) {
-    document.querySelectorAll("button")[index].addEventListener("click", function () {
-        alert("i got clicked");
+for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        
+        this.style.color="white";
 
     });
     
 }
+
+// audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
 
 
